0b2d66876489a9266c8990155d721a1a15aaf62e
Thuso <48202895+thuso@users.noreply.github.com>
2020-10-15 00:41:39 +0200

    finished api

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -19,8 +19,29 @@ namespace Core.Specifications
 
         public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
 
+        public Expression<Func<T, object>> OrderBy { get; private set; }
+
+        public Expression<Func<T, object>> OrderByDescending { get; private set; }
+
+        public int Take { get; private set; }
+
+        public int Skip { get; private set; }
+
+        public bool IsPagingEnabled { get; private set; }
+
         protected void AddInclude( Expression<Func<T, object>> includeExpression){
             Includes.Add(includeExpression);
+        }
+        protected void AddOrderBy(Expression<Func<T, object>> orderByExpession){
+            OrderBy = orderByExpession;
+        }
+        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpession){
+            OrderByDescending = orderByDescExpession;
+        }
+        protected void ApplyPaging( int skip, int take){
+            Skip = skip;
+            Take = take;
+            IsPagingEnabled = true;
         }
     }
 }
