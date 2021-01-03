0b52dd35b9e2e497b23441aa15e0543c4ba259e7
Thuso <thuso@abanganimedia.co.za>
2021-01-03 02:39:09 +0200

    added API validations: basket and identity

--- API/Extensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -9,6 +9,22 @@ namespace API.Extensions
         public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services){
              services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "SkiNet API", Version = "v1"});
+
+                var sercuritySchema = new OpenApiSecurityScheme{
+                    Description = "JWT Auth Scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    Reference = new OpenApiReference{
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer", sercuritySchema);
+                var SecurityRequirement = new OpenApiSecurityRequirement{{sercuritySchema, new []{"Bearer"}}};
+                c.AddSecurityRequirement(SecurityRequirement);
             });
 
             return services;
